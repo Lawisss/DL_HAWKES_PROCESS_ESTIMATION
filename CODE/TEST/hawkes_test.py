a348698c30229cdce51ba1bed06428c2d0c51c0e
Lawisss <nicolas.girard22@outlook.com>
2023-04-20 11:49:34 +0200

    discretisation tests

--- CODE/TEST/hawkes_test.py
+++ CODE/TEST/hawkes_test.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, call
 
 import numpy as np
 import Hawkes as hk
+from pytest import approx
 
 import VARIABLES.hawkes_var as hwk
-from HAWKES.hawkes import hyper_params_simulation, hawkes_simulation, hawkes_simulations, hawkes_estimation
+from HAWKES.hyperparameters import hyper_params_simulation
+from HAWKES.hawkes import hawkes_simulation, hawkes_simulations, hawkes_estimation
+from HAWKES.discretisation import discretise, temp_func, find_stepsize, jump_times
 
 
 # Hyperparameters simulation test function
@@ -137,3 +140,119 @@ def test_hawkes_estimation(mock_hawkes_estimation) -> None:
     assert len(t_transform) == len(interval_transform)
     assert isinstance(metrics, dict)
     assert all(key in metrics for key in ['Event(s)', 'Parameters', 'Branching Ratio', 'Log-Likelihood', 'AIC'])
+
+
+# Discretisation test function
+
+@patch('UTILS.utils.write_parquet')
+def test_discretise(mock_write_parquet) -> None:
+
+    """
+    Test function for Hawkes processes discretisation function
+
+    Args:
+        mock_write_parquet (MagicMock): Mock for write_parquet function
+
+    Returns:
+        None: Function does not return anything
+    """
+
+    # Called function
+    counts = discretise(np.random.rand(5, 1000))
+
+    # Asserted type/shape
+    assert counts.shape == (5, 1000)
+    assert counts.dtype == np.float32
+
+    # Asserted function calling
+    mock_write_parquet.assert_called_once_with(counts, columns=np.arange(hwk.TIME_HORIZON, dtype=np.int32).astype(str), filename='binned_hawkes_simulations.parquet')
+
+
+# Minimum stepsize test function
+
+def test_temp_func() -> None:
+
+    """
+    Test function for minimum stepsize between events computation
+
+    Args:
+        None: This function contains no arguments
+
+    Returns:
+        None: Function does not return anything
+    """
+
+    # Initialized parameters/Asserted results (case n°1)
+    jump_times = np.array([1.2, 2.3, 4.5, 5.6])
+    expected_result = 0.9
+    result = temp_func(jump_times)
+
+    assert result == expected_result
+
+    # Initialized parameters/Asserted results (case n°2)
+    jump_times = np.array([])
+    expected_result = 10.0
+    result = temp_func(jump_times)
+
+    assert result == expected_result
+
+
+# temp_func(x, hwk.TIME_HORIZON) minimum test function
+
+@patch('HAWKES.discretisation.temp_func')
+def test_find_stepsize(mock_temp_func) -> None:
+
+    """
+    Test function for temp_func(x, hwk.TIME_HORIZON) minimum computation
+
+    Args:
+        None: This function contains no arguments
+
+    Returns:
+        None: Function does not return anything
+    """
+
+    # Initialized parameters
+    jump_times = np.array([1, 2, 3, 4, 5])
+
+    # Mock configuration
+    mock_temp_func.side_effect = [1, 2, 3, 4]
+
+    # Called function
+    result = find_stepsize(jump_times)
+
+    # Asserted calls
+    mock_temp_func.assert_has_calls([call(1, hwk.TIME_HORIZON), call(2, hwk.TIME_HORIZON),
+                                     call(3, hwk.TIME_HORIZON), call(4, hwk.TIME_HORIZON)])
+    
+    # Global minimum = 1
+    assert result == 1  
+
+
+# Jump times test function
+
+@patch("numpy.random.uniform", return_value=np.array([0.25, 0.4, 0.8, 0.6]))
+def test_jump_times(mock_uniform) -> None:
+
+    """
+    Test function for jump times computation
+
+    Args:
+        None: This function contains no arguments
+
+    Returns:
+        None: Function does not return anything
+    """
+
+    # Initialized parameters
+    h = np.array([0, 1, 2, 0, 3, 0, 0, 1, 0])
+    expected_result = np.array([0.25, 0.4, 0.4, 0.5, 0.6, 0.8, 0.8, 0.9])
+
+    # Called function
+    result = jump_times(h)
+
+    # Asserted results
+    assert result == approx(expected_result, rel=1e-6)
+
+    # Asserted mock
+    mock_uniform.assert_called_once_with(0.5, 0.6, size=(3,))
