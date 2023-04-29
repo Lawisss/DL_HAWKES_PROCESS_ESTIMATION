1f546db61438fd64077844cd90119bdcbc29c772
Lawisss <nicolas.girard22@outlook.com>
2023-04-29 12:36:11 +0200

    setup modified

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
+import io
+import os
 from setuptools import setup, find_packages
+
+
+def read(*paths, **kwargs):
+    
+    """
+    Read the contents of a text file safely
+    
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md")
+    ...
+    
+    """
+    
+    content = ""
+    
+    with io.open(os.path.join(os.path.dirname(__file__), *paths), encoding=kwargs.get("encoding", "utf8"),) as open_file:
+        content = open_file.read().strip()
+        
+    return content
+
+
+def read_requirements(path):
+    return [line.strip() for line in read(path).split("\n") if not line.startswith(('"', "#", "-", "git+"))]
 
 setup(
     name='DL_HAWKES_PROCESS_ESTIMATION',
@@ -6,14 +32,17 @@ setup(
     description='DL & HAWKES PROCESS ESTIMATION',
     author='Nicolas Girard',
     author_email='nico.girard22@gmail.com',
-    long_description=open('README.md').read(),
+    url='https://github.com/Lawisss',
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    packages=find_packages(),
-    install_requires=['numpy', 'pandas', 
-                      'hawkes', 'torch>=1.9.0', 
-                      'tensorflow>=2.0', 'tqdm', 
-                      'torchinfo'],
+    keywords='development, setup, setuptools',
+    package_dir={'': 'SRC'},
+    packages=find_packages(where='SRC'),
+    python_requires='>=3.7',
+    install_requires=read_requirements("requirements.txt"),
     extras_require={'mpi': ['mpi4py'], 'test': ['pytest', 'pytest-cov', 'codecov']},
+    scripts=[],
+    entry_points={},
     classifiers=['Development Status :: 3 - Alpha',
                  'Environment :: Console',
                  'Environment :: Win32 (MS Windows)',
